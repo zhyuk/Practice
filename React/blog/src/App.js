7c7aca1606d599a79466cf37cf16b3b44ffb6b8c
zhyuk <pc9445@naver.com>
2024-08-29 22:25:11 +0900

    240829

--- React/blog/src/App.js
+++ React/blog/src/App.js
@@ -1,17 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
-  let post = '강남 우동 맛집'; // 서버에서 가져온 데이터라고 가정
-  // document.querySelector('h4').innerHTML = post;  -> JS 내에서 post 변수의 값을 넣을 때 작성하는 코드
+  let post = '강남 우동 맛집';
+  // 자료를 잠
+  let [글제목1, b] = useState(['남자 코트 추천', '여자 코트 추천', '강남 우동 맛집']);
+  // let [logo, setLogo] = useState('ReactBlog');
 
   return (
     <div className="App">
-      <div className='nav'>
-        <h4 style={{color : 'red', fontSize: '16px'}}>BLOG</h4>
+      <div className="black-nav">
+        <h4>ReactBlog</h4>
       </div>
-      <h4>{post}</h4>
+      <p style={{color: 'blue', fontWeight: 'Bold'}}>{post}</p>
+      <div className="list">
+        <h4>{글제목1[0]}</h4>
+        <p>2024년 08월 29일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목1[1]}</h4>
+        <p>2024년 08월 29일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목1[2]}</h4>
+        <p>2024년 08월 29일 발행</p>
+      </div>
     </div>
   );
 }
