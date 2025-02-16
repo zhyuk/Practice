4e0a26d65af8336c65dcb4f16b4dde6406419cb4
zhyuk <pc9445@naver.com>
2025-02-16 22:31:34 +0900

    250216

---
+++ js/njz.js
@@ -0,0 +1,12 @@
+// 사진 클릭 시 ajax
+function getMemberInfo(memberName) {
+    const profile = [
+        document.getElementsByClassName("minji")[0],
+        document.getElementsByClassName("hani")[0],
+        document.getElementsByClassName("danielle")[0],
+        document.getElementsByClassName("haerin")[0],
+        document.getElementsByClassName("hyein")[0]
+    ];
+    const modal = document.getElementById("modal");
+}
+
